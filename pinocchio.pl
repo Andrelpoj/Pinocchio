8603d7b9e918c24e068037ba4bb1e9e20cdd86f3
andrelpoj <andrelpoj@gmail.com>
2018-05-30 21:30:22 -0300

    First commit

---
+++ pinocchio.pl
@@ -0,0 +1,34 @@
+age(gepetto,old).
+age(pinocchio,young).
+occupation(gepetto,carpenter).
+carved(gepetto,pinocchio).
+puppet(pinocchio).
+man(pinocchio).
+wish(gepetto,"pinocchio to be a real boy").
+fullfilled_wish(fairy,gepetto).
+who_what(fairy,"fullfilled gepetto’s wish").
+alive(gepetto).
+alive(pinocchio).
+woman(fairy).
+want(fairy,"if pinocchio was to be a real boy", "he must always be a good boy").
+love(gepetto,pinocchio).
+naughty(pinocchio).
+liar(pinocchio).
+nose_grow(pinocchio,"when he lies").
+promised(pinocchio,"to be a good boy from the next time,"when he lies").
+join(pinocchio,circus).
+misses(pinocchio,gepetto).
+swallowed(whale, gepetto).
+swallowed(whale, pinocchio).
+what_where("pinocchio and gepetto were happy to see each other","whale’s belly").
+make_plan(gepetto,"tickle the whale’s stomach").
+make_plan(pinocchio,"tickle the whale’s stomach").
+what_when("pinocchio and gepetto slipped out","whale sneezed").
+reached_home_safely("gepetto").
+reached_home_safely("pinocchio").
+who_what_how(pinocchio,"reached home", safely).
+who_what_how(gepetto,"reached home",safely).
+who_what(fairy,"was pleased with pinocchio’s bravery").
+who_what_why(the fairy,"turned pinocchio into a real boy","because she was pleased with pinocchio").
+happy(pinocchio).
+happy(gepetto).
