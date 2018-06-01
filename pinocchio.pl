bf2efcc03667168ec66e91a6d32736d148a18c8f
andrelpoj <andrelpoj@gmail.com>
2018-06-01 20:40:53 -0300

    Adição de regras

--- pinocchio.pl
+++ pinocchio.pl
@@ -3,10 +3,10 @@ age(pinocchio,young).
 occupation(gepetto,carpenter).
 carved(gepetto,pinocchio).
 puppet(pinocchio).
+man(gepetto).
 man(pinocchio).
 wish(gepetto,"pinocchio to be a real boy").
 fullfilled_wish(fairy,gepetto).
-who_what(fairy,"fullfilled gepetto’s wish").
 alive(gepetto).
 alive(pinocchio).
 woman(fairy).
@@ -15,7 +15,7 @@ love(gepetto,pinocchio).
 naughty(pinocchio).
 liar(pinocchio).
 nose_grow(pinocchio,"when he lies").
-promised(pinocchio,"to be a good boy from the next time,"when he lies").
+promised(pinocchio,"to be a good boy from the next time","when he lies").
 join(pinocchio,circus).
 misses(pinocchio,gepetto).
 swallowed(whale, gepetto).
@@ -24,11 +24,15 @@ what_where("pinocchio and gepetto were happy to see each other","whale’s belly")
 make_plan(gepetto,"tickle the whale’s stomach").
 make_plan(pinocchio,"tickle the whale’s stomach").
 what_when("pinocchio and gepetto slipped out","whale sneezed").
+what_when("pinocchio's nose grows","pinocchio lies").
 reached_home_safely("gepetto").
 reached_home_safely("pinocchio").
+who_what(fairy,"was pleased with pinocchio’s bravery").
+who_what(fairy,"fullfilled gepetto’s wish").
 who_what_how(pinocchio,"reached home", safely).
 who_what_how(gepetto,"reached home",safely).
-who_what(fairy,"was pleased with pinocchio’s bravery").
 who_what_why(the fairy,"turned pinocchio into a real boy","because she was pleased with pinocchio").
 happy(pinocchio).
 happy(gepetto).
+boy(X):-man(X),age(X,young).
+trust(X):-not(liar(X)).
