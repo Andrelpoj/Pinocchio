bd87668eb34624545fbc00b47ccd2fac6236931f
vipkry <lolpsxvipkry2@gmail.com>
2018-06-02 00:56:54 -0300

    fixing utf-8 and typo

--- pinocchio.pl
+++ pinocchio.pl
@@ -20,18 +20,18 @@ join(pinocchio,circus).
 misses(pinocchio,gepetto).
 swallowed(whale, gepetto).
 swallowed(whale, pinocchio).
-what_where("pinocchio and gepetto were happy to see each other","whale’s belly").
-make_plan(gepetto,"tickle the whale’s stomach").
-make_plan(pinocchio,"tickle the whale’s stomach").
+what_where("pinocchio and gepetto were happy to see each other","whale's belly").
+make_plan(gepetto,"tickle the whale's stomach").
+make_plan(pinocchio,"tickle the whale's stomach").
 what_when("pinocchio and gepetto slipped out","whale sneezed").
 what_when("pinocchio's nose grows","pinocchio lies").
 reached_home_safely("gepetto").
 reached_home_safely("pinocchio").
-who_what(fairy,"was pleased with pinocchio’s bravery").
-who_what(fairy,"fullfilled gepetto’s wish").
+who_what(fairy,"was pleased with pinocchio's bravery").
+who_what(fairy,"fullfilled gepetto's wish").
 who_what_how(pinocchio,"reached home", safely).
 who_what_how(gepetto,"reached home",safely).
-who_what_why(the fairy,"turned pinocchio into a real boy","because she was pleased with pinocchio").
+who_what_why(fairy, "turned pinocchio into a real boy", "because she was pleased with pinocchio").
 happy(pinocchio).
 happy(gepetto).
 boy(X):-man(X),age(X,young).
