5389978eeee09178cb9eb598d277cd6bd2215c97
vipkry <lolpsxvipkry2@gmail.com>
2018-06-03 12:03:42 -0300

    removendo arquivos extras

--- pinocchio_real_boy.pl
+++
@@ -1,7 +0,0 @@
-% Fatos
-
-who_what(fairy,"was pleased with pinocchio's bravery").
-who_what(fairy,"fullfilled gepetto's wish").
-who_what_why(fairy, "turned pinocchio into a real boy", "because she was pleased with pinocchio").
-happy(pinocchio).
-happy(gepetto).
